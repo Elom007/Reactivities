b0accb4018c506fbd0f7f9a6568b0cf33f86c914
Emmanuel Elom <117633808+Elom-E@users.noreply.github.com>
2023-09-18 09:38:52 -0400

    End of Phase 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,24 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-// builder.Services.AddCors(opt => {
-//     opt.AddPolicy("CorsPolicy", policy => 
-//     {
-//         policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-//     });
-// });
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -33,7 +22,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors("CorsPolicy");
 app.UseRouting();
-app.UseCors(policy => policy.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader());
+// app.UseCors(policy => policy.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader());
 app.UseAuthorization();
 
 app.MapControllers();
